56258f92c27bbfe612daae031aa388b2b18baa83
Terje Innerdal <terje.innerdal@gmail.com>
2025-12-13 18:44:00 +0100

    Catches docker exit code

--- NextPVR.ps1
+++ NextPVR.ps1
@@ -1,7 +1,7 @@
 # This script starts the NextPVR container using Docker.
 # NextPVR is a personal video recorder application.
 
-docker run -d `
+$out = docker run -d `
     --name nextpvr `
     --volume "c:/nextpvr/config:/config" `
     --volume "c:/nextpvr/videos:/videos" `
@@ -10,5 +10,8 @@ docker run -d `
     --publish 8866:8866 `
     --publish 16891:16891/udp `
     nextpvr/nextpvr_amd64:stable
+
+$code = $LASTEXITCODE
+if ($code -ne 0) {throw "docker failed (code $code):$out").[3][5]
 
 Write-Host "NextPVR container is starting. Access it at http://localhost:8866"
