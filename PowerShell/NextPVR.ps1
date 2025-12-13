627308889d8b262253523bb10bfc3d1b71251aed
Terje Innerdal <terje.innerdal@gmail.com>
2025-12-13 18:46:44 +0100

    Moved NextPVR.ps1 to the Powershell folder

--- PowerShell/NextPVR.ps1
+++ PowerShell/NextPVR.ps1
@@ -1,14 +1,17 @@
 # This script starts the NextPVR container using Docker.
 # NextPVR is a personal video recorder application.
 
-docker run -d `
+$out = docker run -d `
     --name nextpvr `
-    --volume "/home/nextpvr/config:/config" `
-    --volume "/media/terje/1TB/nextpvr/videos:/videos" `
-    --volume "/media/terje/1TB/nextpvr/buffer:/buffer" `
+    --volume "c:/nextpvr/config:/config" `
+    --volume "c:/nextpvr/videos:/videos" `
+    --volume "c:/nextpvr/buffer:/buffer" `
     --restart unless-stopped `
     --publish 8866:8866 `
     --publish 16891:16891/udp `
     nextpvr/nextpvr_amd64:stable
+
+$code = $LASTEXITCODE
+if ($code -ne 0) {throw "docker failed (code $code):$out").[3][5]
 
 Write-Host "NextPVR container is starting. Access it at http://localhost:8866"
