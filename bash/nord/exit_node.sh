5c4c9bd8eabcc40a18e01f0e54652f6d6cefc960
terje <terje.everest@gmail.com>
2026-03-30 15:58:21 +0200

    Added script to set up the RaspberryPI as an exit node

--- bash/nord/exit_node.sh
+++ bash/nord/exit_node.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/bash
 
+#! /usr/bin/bash
+
+nordvpn set lan-discovery off
+nordvpn meshnet set nickname BigAssBerryPI
+
+nordvpn meshnet peer routing allow S22
+nordvpn meshnet peer routing allow Tab8
+nordvpn meshnet peer routing allow DELL
+nordvpn meshnet peer routing allow HPMint
+nordvpn meshnet peer routing allow sunndal
+
 # You must explicitly grant permission for other devices to route through it.
 nordvpn meshnet peer allow-exit-node set terje.innerdal@gmail.com
