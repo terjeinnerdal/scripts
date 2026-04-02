0749662491ae12358ee8f40687170f143c8bee00
terje <terje.everest@gmail.com>
2026-04-02 12:28:10 +0200

    Added README.md explaining meshnet nicknames

--- bash/nord/config.sh
+++ bash/nord/config.sh
@@ -1,17 +1,38 @@
 #! /usr/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Error: No argument provided."
+    exit 1
+fi
+
+echo "The argument is: $1"
+
+NICKNAME=$1
+
 nordvpn set meshnet on
+nordvpn meshnet set nickname $NICKNAME
+
 nordvpn set notify on
-nordvpn set autoconnect on BigAssBerryPi
+
 nordvpn set pq off
 nordvpn set lan-discovery off
 nordvpn set technology nordlynx
 
-# Auto-accept files shared from peers
-nordvpn meshnet peer fileshare allow hp
-nordvpn meshnet peer auto-accept enable hp
-nordvpn meshnet peer fileshare allow Pixel
-nordvpn meshnet peer auto-accept enable Pixel
-nordvpn meshnet peer fileshare allow Tab8
-nordvpn meshnet peer auto-accept enable Tab8
+# Set auto-connect for these devices
+nordvpn set autoconnect on mesh-raspberry
+nordvpn set autoconnect on mesh-hp
+nordvpn set autoconnect on mesh-dell
+nordvpn set autoconnect on mesh-tab8
+nordvpn set autoconnect on mesh-pixel
 
+# Auto-accept files shared from peers
+nordvpn meshnet peer fileshare allow mesh-hp
+nordvpn meshnet peer auto-accept enable mesh-hp
+nordvpn meshnet peer fileshare allow mesh-dell
+nordvpn meshnet peer auto-accept enable mesh-dell
+nordvpn meshnet peer fileshare allow mesh-pixel
+nordvpn meshnet peer auto-accept enable mesh-pixel
+nordvpn meshnet peer fileshare allow mesh-tab8
+nordvpn meshnet peer auto-accept enable mesh-tab8
+nordvpn meshnet peer fileshare allow mesh-raspberry
+nordvpn meshnet peer auto-accept enable mesh-raspberry
--- bash/nord/exit_node.sh
+++ bash/nord/exit_node.sh
@@ -1,17 +1,19 @@
 #! /usr/bin/bash
 
-nordvpn set mesh on
+# This script lets other peers route through the RaspberryPI
 
-nordvpn set lan-discovery off
-
+nordvpn set meshnet on
 nordvpn meshnet set nickname BigAssBerryPI
+
+# Other meshnet peers will not be able to access local network devices
+nordvpn set lan-discovery off
 
 #nordvpn meshnet peer routing allow S22
 #nordvpn meshnet peer routing allow Tab8
-nordvpn meshnet peer routing allow DELL
+nordvpn meshnet peer routing allow pi-dell
 #nordvpn meshnet peer routing allow HPMint
 #nordvpn meshnet peer routing allow sunndal
 nordvpn meshnet peer routing allow Pixel
 
 # You must explicitly grant permission for other devices to route through it.
-nordvpn meshnet peer allow-exit-node set terje.innerdal@protonmail.com
+# nordvpn meshnet peer allow-exit-node set terje.innerdal@protonmail.com
